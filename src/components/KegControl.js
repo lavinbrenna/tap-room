e049a93d738c11165f30334d891969a624226ed4
Brenna Lavin <lavinbrenna@gmail.com>
2022-04-22 11:15:27 -0700

    add keg control, keg details, compile app

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,18 +1,78 @@
-// import React from 'react';
-// import NewKegForm from './NewKegForm';
-// import KegList from './KegList';
-// import KegDetail from './KegDetail';
+import React from 'react';
+import NewKegForm from './NewKegForm';
+import KegList from './KegList';
+import KegDetail from './KegDetails';
 
-// class KegControl extends React.Component{
-//   constructor(props){
-//     super(props);
-//     this.state ={
-//       formVisibleOnPage: false,
-//       mainKegList: [],
-//       selectedKeg: null
-//     };
-//   }
-//   handleClick = () => {
+class KegControl extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      formVisibleOnPage: false,
+      mainKegList: [],
+      selectedKeg: null
+    };
+  }
+  handleClick = () => {
+    if(this.state.selectedKeg != null){
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null
+      });
+    }else{
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
 
-//   }
-// }
+  handleAddingNewKegToList = (newKeg) => {
+    const newMainKegList = this.state.mainKegList.concat(newKeg);
+    this.setState({mainKegList: newMainKegList, formVisibleOnPage: false});
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.mainKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
+  }
+
+  handleDeletingKeg = (id) => {
+    const newMainKegList = this.state.mainKegList.filter(keg => keg.id !== id);
+    this.setState({
+      mainKegList: newMainKegList,
+      selectedKeg: null
+    });
+  }
+
+  // handleSellingPint = (id) => {
+  //   const selectedKeg = this.state.mainKegList.filter(keg=> keg.id === id)[0];
+  //   this.setState({
+  //     mainKegList: mainKegList,
+  //     selectedKeg: (selectedKeg.pintsLeft - 1)
+  //   })
+  //   console.log("sold pint");
+  // }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if(this.state.selectedKeg != null){
+      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} onClickingDelete = {this.handleDeletingKeg} />
+      buttonText = "Return to Keg List";
+    }
+    else if(this.state.formVisibleOnPage){
+      currentlyVisibleState = <NewKegForm onNewKegCreation = {this.handleAddingNewKegToList}/>
+      buttonText = "Return to Keg List";
+    }
+    else{
+      currentlyVisibleState = <KegList kegList={this.state.mainKegList} onKegSelection = {this.handleChangingSelectedKeg}/>
+      buttonText = "Add Keg";
+    }
+    return(
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick = {this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    )
+  }
+}
+export default KegControl;
---
+++ src/components/KegDetails.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegDetail(props){
+  const { keg, onClickingDelete } = props;
+  return(
+    <React.Fragment>
+      <h1>Keg Detail</h1>
+      <h3>{keg.name} - {keg.flavor}</h3>
+      <h5>{keg.brand}</h5>
+      <p><em>Pint price: ${keg.price}.00</em></p>
+      <p><em>Pints left: {keg.pintsLeft}</em></p>
+      <button onClick={()=> onClickingDelete(keg.id)}>Delete Keg</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+KegDetail.propTypes = {
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func
+};
+
+export default KegDetail;
