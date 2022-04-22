d62f83aa1e27b2d02dcc85817fb2da1a5e1e9bcc
Brenna Lavin <lavinbrenna@gmail.com>
2022-04-22 12:43:13 -0700

    dynamically add sell pint button to each keg

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -3,9 +3,11 @@ import Keg from "./Keg";
 import PropTypes from "prop-types";
 
 function KegList(props){
-  return (<React.Fragment>
+  return (
+  <React.Fragment>
     <hr/>
     {props.kegList.map((keg, index)=>
+    <React.Fragment>
       <Keg
         whenKegClicked = {props.onKegSelection}
         name = {keg.name}
@@ -14,15 +16,21 @@ function KegList(props){
         flavor = {keg.flavor}
         pintsLeft = {keg.pintsLeft}
         id={keg.id}
-        key={keg.id}/>
+        key={keg.id}
+        />
+        <button>Sell Pint</button>
+        <hr/>
+      </React.Fragment>
     )}
+    
   </React.Fragment>
   );
 }
 
 KegList.propTypes ={
   kegList: PropTypes.array,
-  onKegSelection: PropTypes.func
+  onKegSelection: PropTypes.func,
+  // onClickingSellPint: PropTypes.func
 };
 
 export default KegList;
