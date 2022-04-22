298657948f74d2d24381d2b83050a1b429a2b075
Brenna Lavin <lavinbrenna@gmail.com>
2022-04-22 10:11:48 -0700

    finish newkegform functionality, add keg component code

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,8 +1,27 @@
-// import React from "react";
-// import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from 'prop-types';
 
-// function Keg(props){
-//   return{
+function Keg(props){
+  return(
+    <React.Fragment>
+      <div onClick = {()=> props.whenKegClicked(props.id)}>
+        <h3>{props.name} - {props.flavor}</h3>
+        <p><em>Pint price : $ {props.price}.00</em></p>
+        <p><em>Pints left: {props.pintsLeft}</em></p>
+        <hr/>
+      </div>
+    </React.Fragment>
+  );
+}
 
-//   }
-// }
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  flavor: PropTypes.string.isRequired,
+  pintsLeft: PropTypes.number,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func,
+};
+
+export default Keg;
--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -8,4 +8,15 @@ function NewKegForm(props){
     event.preventDefault();
     props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.price.value, pintsLeft: 124, id: v4()});
   }
+  return(
+    <React.Fragment>
+      <Form formSubmissionHandler= {handleNewKegFormSubmission} buttonText="Submit"/>
+    </React.Fragment>
+  );
 }
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
